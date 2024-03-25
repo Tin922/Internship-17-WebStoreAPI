4e50a15df097e9da374d06e5ae57a5bd165863bc
Tin922 <tinjuric92@gmail.com>
2024-03-25 15:35:12 +0100

    added user.entity and added api responses

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  userName: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  isAdmin: boolean;
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+}
