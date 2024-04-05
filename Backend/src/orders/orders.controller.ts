907db8113cb1cd471b5d0fa1046ac597cbe8bed1
Tin922 <tinjuric92@gmail.com>
2024-04-05 16:01:34 +0200

    added AuthGuard to Post and Get for Orders

--- Backend/src/orders/orders.controller.ts
+++ Backend/src/orders/orders.controller.ts
@@ -6,34 +6,41 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrdersEntity } from './entities/order.entity';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UserAuthGuard } from 'src/users/user-auth.guard';
 
 @Controller('orders')
 @ApiTags('Orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @UseGuards(UserAuthGuard)
   @Post()
   @ApiCreatedResponse({ type: OrdersEntity })
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  create(@Req() { user }) {
+    return this.ordersService.create(user.id);
   }
 
+  @UseGuards(UserAuthGuard)
   @Get()
   @ApiOkResponse({ type: OrdersEntity, isArray: true })
-  findAll() {
-    return this.ordersService.findAll();
+  findAll(@Req() { user }) {
+    return this.ordersService.findAll(user.id);
   }
 
+  @UseGuards(UserAuthGuard)
   @Get(':id')
   @ApiOkResponse({ type: OrdersEntity })
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number, @Req() { user }) {
+    return this.ordersService.findOne(id, user.id);
   }
 
   @Patch(':id')
--- Backend/src/orders/orders.service.ts
+++ Backend/src/orders/orders.service.ts
@@ -6,16 +6,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class OrdersService {
   constructor(private prisma: PrismaService) {}
-  create(createOrderDto: CreateOrderDto) {
-    return this.prisma.orders.create({ data: createOrderDto });
+  create(userId: number) {
+    return this.prisma.orders.create({ data: { userId } });
   }
 
-  findAll() {
-    return this.prisma.orders.findMany();
+  findAll(userId: number) {
+    return this.prisma.orders.findMany({ where: { userId } });
   }
 
-  findOne(id: number) {
-    return this.prisma.orders.findUnique({ where: { id } });
+  findOne(id: number, userId: number) {
+    return this.prisma.orders.findUnique({ where: { id, userId } });
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
