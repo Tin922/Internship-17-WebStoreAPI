6f861feaafe8e9ea71b552f824554aed89d042a7
Tin922 <tinjuric92@gmail.com>
2024-04-12 16:52:27 +0200

    added UserProvider

---
+++ apps/Frontend/src/providers/UserProvider/UserProvider.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = (user) => {
+    setUser(user);
+    setIsLoggedIn(true);
+  };
+
+  const contextValue = {
+    user,
+    isLoggedIn,
+    login,
+  };
+
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  return useContext(UserContext);
+};
+export { UserProvider };
